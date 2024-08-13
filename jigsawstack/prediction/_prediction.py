78cec92aa34fcdff5c321e6ba8839860219e3058
Narcisse <mechikaegon@gmail.com>
2024-08-13 19:13:11 +0100

    Clean up

---
+++ jigsawstack/prediction/_prediction.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict, List, Union, cast
+from typing_extensions import NotRequired, TypedDict
+from jigsawstack import request
+from typing import List, Union
+
+class Dataset(TypedDict):
+    value : int
+    """
+    The value of the dataset.
+    """
+
+    date : str
+    """
+    The date of the dataset.
+    """
+
+
+class PredictionParams(TypedDict):
+    dataset: List[Dataset]
+    """
+    The dataset to make predictions on. This is an array of object with keys date and value. See example below for more information.
+    """
+    steps: int
+    """
+    The number of predictions to make. The defualt is 5. 
+    """
+
+class PredictionResponse(TypedDict):
+    success: bool
+    """
+    Indicates whether the translation was successful.
+    """
+    prediction: object
+
+def prediction(params: PredictionParams) -> PredictionResponse:
+    path = "/ai/prediction"
+    resp = request.Request(path=path,params=cast(Dict[Any, Any], params),verb="post").perform_with_content()
+    return resp
---
+++ tests/test_prediction.py
@@ -0,0 +1,50 @@
+from unittest.mock import MagicMock
+import unittest
+
+from jigsawstack.exceptions import JigsawStackError
+
+import jigsawstack
+import jigsawstack.prediction
+import jigsawstack.prediction._prediction
+import jigsawstack.sentiment
+import jigsawstack.sentiment._sentiment
+import jigsawstack.translate
+import jigsawstack.translate._translate
+
+# flake8: noqa
+
+
+class TestPredictionAPI(unittest.TestCase):
+
+    def test_sentiment_response_success(self) -> None:
+        params = {
+            "dataset": [  {
+    "date": "2023-10-10",
+    "value": 10,
+  },
+  {
+    "date": "2023-09-10",
+    "value": 5,
+  },{
+    "date": "2023-08-10",
+    "value": 40,
+  },
+  {
+    "date": "2023-07-10",
+    "value": 30,
+  },
+  {
+    "date": "2023-06-10",
+    "value": 10,
+  },
+  {
+    "date": "2023-05-10",
+    "value": 10,
+  }
+  ]
+        }
+        try:
+            result = jigsawstack.prediction._prediction.prediction(params)
+            assert result["success"] == True
+        except JigsawStackError as e:
+            assert e.message == "Failed to parse API response. Please try again."
