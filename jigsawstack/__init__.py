12f8752c0d70e76b6d8d99f89bce35824fb1ffd1
Narcisse <mechikaegon@gmail.com>
2024-08-18 19:12:34 +0100

    Clean up

--- jigsawstack/__init__.py
+++ jigsawstack/__init__.py
@@ -1,17 +1,4 @@
 import os
-from .web.web import Web
-from .audio.audio import Audio
-from .validate.validate import Validate
-from .vision.vision import Vision
-from .vision.vision import Vision
-from .store.kv import KV
-from .store.file import File
-from .translate._translate import Translate
-from .prediction._prediction import Prediction
-from .search._search import Search
-from .summary._summary import Summary
-from .sentiment._sentiment import Sentiment
-from .sql._sql import SQL
 from ._client import JigsawStack
 
 # Config vars
