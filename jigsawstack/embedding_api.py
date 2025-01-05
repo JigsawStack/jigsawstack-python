b84878ad55c54b54054e6f21adf588e3138cd43e
Narcisse <mechikaegon@gmail.com>
2025-01-05 15:39:36 +0100

    clean up

---
+++ jigsawstack/embedding_api.py
@@ -0,0 +1,80 @@
+from typing import Any, Dict, List, Union, cast, Literal
+from typing_extensions import NotRequired, TypedDict
+from .request import Request, RequestConfig
+from .async_request import AsyncRequest
+from typing import List, Union
+from ._config import ClientConfig
+
+
+class EmbeddingParams(TypedDict):
+    text: Union[str, List[str]]
+    """
+    The text to summarize.
+    """
+
+    type: NotRequired[Literal["text", "points"]]
+
+    """
+   The summary result type. Supported values are: text, points
+    """
+    url: NotRequired[str]
+    file_store_key: NotRequired[str]
+    max_points: NotRequired[int]
+    max_characters: NotRequired[int]
+
+
+class EmbeddingResponse(TypedDict):
+    success: bool
+    """
+    Indicates whether the translation was successful.
+    """
+    summary: str
+    """
+    The summarized text.
+    """
+
+
+class Embedding(ClientConfig):
+
+    config: RequestConfig
+
+    def __init__(
+        self,
+        api_key: str,
+        api_url: str,
+        disable_request_logging: Union[bool, None] = False,
+    ):
+        super().__init__(api_key, api_url, disable_request_logging)
+        self.config = RequestConfig(
+            api_url=api_url,
+            api_key=api_key,
+            disable_request_logging=disable_request_logging,
+        )
+
+
+class AsyncEmbedding(ClientConfig):
+
+    config: RequestConfig
+
+    def __init__(
+        self,
+        api_key: str,
+        api_url: str,
+        disable_request_logging: Union[bool, None] = False,
+    ):
+        super().__init__(api_key, api_url, disable_request_logging)
+        self.config = RequestConfig(
+            api_url=api_url,
+            api_key=api_key,
+            disable_request_logging=disable_request_logging,
+        )
+
+    async def execute(self, params: EmbeddingParams) -> EmbeddingResponse:
+        path = "/ai/embedding"
+        resp = await AsyncRequest(
+            config=self.config,
+            path=path,
+            params=cast(Dict[Any, Any], params),
+            verb="post",
+        ).perform_with_content()
+        return resp
